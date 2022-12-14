6698876717f8d7f822a8f1776a1795959689b802
BrauJR <115375487+BrauJR@users.noreply.github.com>
2022-12-13 22:19:36 -0600

    init commit

---
+++ stripe-keys.js
@@ -0,0 +1,4 @@
+export default {
+    public: "pk_test_51LpjGiB5GPDy3ChGB7td40hqP039MS38iXXdSWXf330172ZTmKLx9ljFDIuV3I8yTbwWHKyK2y8n4b5SR0eLXmM700OCZ19Zcw",
+    secret: "sk_test_51LpjGiB5GPDy3ChGcJ9BKPRhcMGZnvwORQ8df99VpV3tXBVGYPZl8evcdbesxHaQJaVxuGVTYDZoXxzRYQDeqmOJ00pDH8ijNN",
+};
